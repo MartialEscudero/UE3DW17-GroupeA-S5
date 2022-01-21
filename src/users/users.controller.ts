661550d960494a7bcfb347023b32c70642567e54
Gabriel DENAJAR <gabriel.denajar@etu.unilim.fr>
2022-01-21 22:49:02 +0100

    Add update and timestamps to document

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,29 +1,35 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Put, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './schemas/user.schema';
 
 @Controller('api')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) { }
 
-  @Post('createuser')
-  async createUser(@Body() createUserDto: CreateUserDto) {
-    await this.usersService.createUser(createUserDto);
-  }
+    @Post('createuser')
+    async createUser(@Body() createUserDto: CreateUserDto) {
+        await this.usersService.createUser(createUserDto);
+    }
 
-  @Get('users')
-  async findAll(): Promise<User[]> {
-    return this.usersService.findAll();
-  }
+    @Get('users')
+    async findAll(): Promise<User[]> {
+        return this.usersService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.usersService.findOne(id);
+    }
 
-  @Delete(':id')
-  removeUser(@Param('id') id: string) {
-    return this.usersService.removeUser(id);
-  }
+    @Delete(':id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.removeUser(id);
+    }
+
+    @Put('updateuser')
+    async update(@Body('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return this.usersService.update(id, updateUserDto);
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,36 +2,46 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
-import { User, UserDocument } from './schemas/user.schema'
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectModel(User.name) private userModel: Model<UserDocument>
-  ) {}
+    constructor(
+        @InjectModel(User.name) private userModel: Model<UserDocument>
+    ) {}
 
-  async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
-    return createdUser.save();
-  }
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const createdUser = new this.userModel(createUserDto);
+        return createdUser.save();
+    }
 
-  async findAll(): Promise<User[]> {
-    return this.userModel.find().exec();
-  }
+    async findAll(): Promise<User[]> {
+        return this.userModel.find().exec();
+    }
 
-  async findOne(id: string) {
-      const user = await this.userModel.findById(id);
+    async findOne(id: string) {
+        const user = await this.userModel.findById(id);
 
-      if (!user) {
-          throw new NotFoundException(`Utilisateur avec l'id : ${id} non trouvé !`)
-      }
+        if (!user) {
+            throw new NotFoundException(`Utilisateur avec l'id : ${id} non trouvé !`)
+        }
 
-      return { user };
-  }
+        return { user };
+    }
 
-  async removeUser(id: string) {
-    const { user } = await this.findOne(id);
+    async removeUser(id: string) {
+        const { user } = await this.findOne(id);
 
-    await user.remove();
-}
+        await user.remove();
+    }
+
+    async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+        const doc = await this.userModel.findById(id).exec();
+        doc.firstname = updateUserDto.firstname;
+        doc.lastname = updateUserDto.lastname;
+        return doc.save();
+        //a another way to update a document
+        //return this.userModel.findByIdAndUpdate({_id: updateUserDto.id}, {$set: {firstname: updateUserDto.firstname, lastname: updateUserDto.lastname}}, {new: true});
+    }
 }
